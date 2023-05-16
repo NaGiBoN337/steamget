8161e02d984a122a33590c224091998f90721a52
Pila <oleg13fedorov@yandex.ru>
2023-05-16 15:11:55 +0300

    add promo

--- steamget/views.py
+++ steamget/views.py
@@ -2,36 +2,48 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from steamget.order_f import QiwiForm
 from steamget.worksql import *
+import logging
 
+logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="a",format="%(asctime)s %(levelname)s %(message)s")
 
 def index(request):
     return render(request,"index.html")
 
-
+#Qazwsxedcf1-\u2036283_Ashibur\u2036283_wallet
 def results(request):
     try:
-        connection = connect_mysql("localhost", "root", "root", "wallet")
-        list_oper_created = select_last_oper(connection, 20)
+        ObSql = workSql("wallet", "localhost", "root", "root")
+        ObSql.select_last_oper(20)
         request1 = QiwiForm("1")
         print(list_oper_created)
         for i in list_oper_created:
             if request1.checkPay(i):
-                update_status(connection,"paid",i)
+                ObSql.update_status("paid",i)
             else:
                 pass
 
     except Exception as e:
+        logging.warning("sql" + str(e))
         print(e)
 
-    return HttpResponse(results)
+    return HttpResponsePermanentRedirect("/")
+    #return HttpResponse("<h2>ошибка:   </h2>")
+   # return HttpResponse(results)
 
 def order(request):#
     login = request.POST.get("login",0)
     money = request.POST.get("money",0)
-    promo = request.POST.get("promo",0)
+    promo = request.POST.get("promo",'base')
 
     try:
-        money = int(money) * 1.20
+        ObSql = workSql("wallet", "localhost", "root", "root")
+        coef = ObSql.promo_for_login(login,promo)
+        print(coef)
+    except Exception as e:
+        logging.warning("sql" + str(e))
+
+    try:
+        money = round(int(money) * (1 + coef),2)
         print(money)
         if len(login) <= 0:
             return HttpResponsePermanentRedirect("/")
@@ -39,16 +51,19 @@ def order(request):#
         return HttpResponsePermanentRedirect("/")
 
 
-    request1 = QiwiForm(str(money))
-    request1.createSignHash()
-    url, orderId = request1.qiwi_request()
+    try: # ошибка на стороне платежки(1 раз словил)
+        request1 = QiwiForm(str(money))
+        request1.createSignHash()
+        url, orderId = request1.qiwi_request()
+    except:
+        return HttpResponsePermanentRedirect("/")
 
     try:
-        connection = connect_mysql("localhost", "root", "root", "wallet")
-        insert(connection,login, money, 0, orderId)
+        ObSql.insert(login, money, 0, orderId)
 
     except Exception as e:
-        print(e)
+        logging.warning("sql" + str(e))
+
 
     return HttpResponsePermanentRedirect(url)
 
--- steamget/worksql.py
+++ steamget/worksql.py
@@ -3,43 +3,85 @@ import pymysql.cursors
 from datetime import datetime
 
 
-def connect_mysql(host, user, password, db):
-    return pymysql.connect(host=host,
-                           port=3306,
-                           user=user,
-                           password=password,
-                           db=db,
-                           charset='utf8mb4',
-                           cursorclass=pymysql.cursors.DictCursor)
+
+class workSql():
+    def __init__(self,bd_name,host,user,password):
+        self.bd_name = bd_name
+        self.host = host
+        self.user = user
+        self.password = password
+        self.con = self.connect_mysql()
+        self.base_coef = self.select_promo('base')['coefficient']
+
+    def connect_mysql(self):
+        return pymysql.connect(host=self.host,
+                               port=3306,
+                               user=self.user,
+                               password=self.password,
+                               db=self.bd_name,
+                               charset='utf8mb4',
+                               cursorclass=pymysql.cursors.DictCursor)
 
 
-def insert(con,login,summ, promo, operId):
-    current_date = datetime.now()
-    query = f"insert into wallet.order (`login`,`summa`,`date`,`OperationId`,`PaymentState`,`promo`) values ('{login}', {summ},'{current_date}','{operId}','Created', {promo});"
+    def insert(self,login,summ, promo, operId):
+        current_date = datetime.now()
+        query = f"insert into {self.bd_name}.order (`login`,`summa`,`date`,`OperationId`,`PaymentState`,`promo`) values ('{login}', {summ},'{current_date}','{operId}','Created', {promo});"
 
-    with con.cursor() as cursor:
-        cursor.execute(query)
-        con.commit()
+        with self.con.cursor() as cursor:
+            cursor.execute(query)
+            self.con.commit()
 
-def select_last_oper(con,past_time):
-    query = f"SELECT * FROM wallet.order where date >= DATE_SUB(NOW() , INTERVAL {past_time} MINUTE) and PaymentState = 'Created';"
+    def select_last_oper(self,past_time):
+        query = f"SELECT * FROM {self.bd_name}.order where date >= DATE_SUB(NOW() , INTERVAL {past_time} MINUTE) and PaymentState = 'Created';"
 
-    with con.cursor() as cursor:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        con.commit()
-        list_oper_id = []
-        for i in result:
-            list_oper_id.append(i['OperationId'])
-        return list_oper_id
-
-
-def update_status(con,status,oper_id):
-    query = f"UPDATE `wallet`.`order` SET `PaymentState` = '{status}' WHERE (`OperationId` = '{oper_id}');"
-    with con.cursor() as cursor:
-        cursor.execute(query)
-        con.commit()
+        with self.con.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            self.con.commit()
+            list_oper_id = []
+            for i in result:
+                list_oper_id.append(i['OperationId'])
+            return list_oper_id
 
 
+    def update_status(self,status,oper_id):
+        query = f"UPDATE `{self.bd_name}`.`order` SET `PaymentState` = '{status}' WHERE (`OperationId` = '{oper_id}');"
+        with self.con.cursor() as cursor:
+            cursor.execute(query)
+            self.con.commit()
+#это говно надо в функцию\процедуру объeденить sql!!!!!!!!!!!!!!!!!!!!!!
+#это говно надо в функцию\процедуру объeденить sql!!!!!!!!!!!!!!!!!!!!!!
+#это говно надо в функцию\процедуру объeденить sql!!!!!!!!!!!!!!!!!!!!!!
+#это говно надо в функцию\процедуру объeденить sql!!!!!!!!!!!!!!!!!!!!!!
+
+    def promo_for_login(self, login, label):
+        promo = self.select_promo(label)
+        if promo:
+            #print(promo)
+            query = f"SELECT count(login) as count from {self.bd_name}.order where promo = {promo['id_promo']} and PaymentState = 'paid' and login = '{login}';"
+            with self.con.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                self.con.commit()
+            if result[0]['count'] < promo['counts_use_for_user']:
+                return promo['coefficient']
+            else:# исчерпал лимит
+                return self.base_coef
+        else:# ошибка промика, такого нет
+            return self.base_coef
 
 
+    def select_promo(self, label):#только если есть и включен
+        query = f"SELECT * FROM {self.bd_name}.tpromo where label = '{label}' and turned = 1;"
+        with self.con.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            self.con.commit()
+            try:#если такого нету
+                return result[0]
+            except:
+                return 0
+
+#
+# new = workSql("wallet","localhost", "root", "root")
+# print(new.promo_for_login('test','you'))
